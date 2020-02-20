ded95a53131fa1300b179f374d5cbbc3708c335a
Wangh7 <978102365@qq.com>
2020-02-20 20:36:30 +0800

    用v-for添加导航栏

--- src/components/common/NavMenu.vue
+++ src/components/common/NavMenu.vue
@@ -3,31 +3,32 @@
     :default-active="'/index'"
     router
     mode="horizontal"
-    @select="handleSelect"
     background-color="#545c64"
     text-color="#fff"
-    active-text-color="#ffd04b">
-    <el-menu-item index="1">处理中心</el-menu-item>
-    <el-submenu index="2">
-      <template slot="title">我的工作台</template>
-      <el-menu-item index="2-1">选项1</el-menu-item>
-      <el-menu-item index="2-2">选项2</el-menu-item>
-      <el-menu-item index="2-3">选项3</el-menu-item>
-      <el-submenu index="2-4">
-        <template slot="title">选项4</template>
-        <el-menu-item index="2-4-1">选项1</el-menu-item>
-        <el-menu-item index="2-4-2">选项2</el-menu-item>
-        <el-menu-item index="2-4-3">选项3</el-menu-item>
-      </el-submenu>
-    </el-submenu>
-    <el-menu-item index="3" disabled>消息中心</el-menu-item>
-    <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+    active-text-color="#ffd04b"
+
+    style="min-width: 1000px;">
+    <el-menu-item v-for="(item,i) in navList" :key="i" :index="item.name">
+      {{ item.navItem }}
+    </el-menu-item>
+    <a href="#nowhere" style="color: #222;float: right;padding: 20px;">更多功能</a>
   </el-menu>
 </template>
 
 <script>
 export default {
-    name: 'NavMenu'
+    name: 'NavMenu',
+    data (){
+      return {
+        navList: [
+          {name: '/index', navItem: '浏览大厅'},
+          {name: '/buy', navItem: '我买到的'},
+          {name: '/sell', navItem: '我卖出的'},
+          {name: '/info', navItem: '个人信息'}
+
+        ]
+      }
+    }
 }
 </script>
 
