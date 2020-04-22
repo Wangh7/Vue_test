038fcd80f44403b8354d6b70f75ef2a845ac13a1
Wangh7 <978102365@qq.com>
2020-04-22 21:08:37 +0800

    修改了主页设计，新增促销信息功能

--- src/components/home/Appindex.vue
+++ src/components/home/Appindex.vue
@@ -19,7 +19,7 @@
           </div>
           <br>
           <el-card
-            v-for="(hotSell,i) in hotSellList"
+            v-for="(hotSell,i) in hotSellList.slice(0,3)"
             :key="i"
             style="width: 100%;height: 100%;margin-bottom: 20px;margin-right: 15px;float: left"
             body-style="padding: 0px"
@@ -38,8 +38,52 @@
 
           </el-card>
         </el-col>
-        <el-col :span="20">
-          折扣信息
+        <el-col :span="18" :offset="2">
+          <el-row>
+            <div style="font-size: 38px;float: left;">最新上架</div>
+          </el-row>
+          <br>
+          <el-row style="height: 100px;">
+            <el-card
+              v-for="(item,i) in recentItemList.slice(0,4)"
+              :key="i"
+              shadow="never"
+              style="height:120px;width: 22%;margin-right: 22px;float: left;background: #d3dce6"
+            >
+              <img style="width: 30%;float: left" :src="'/static/hot/new.png'">
+              <div style="font-size: 30px;text-align: center;margin-top: 5px;">{{item.typeName}}</div>
+              <div style="text-align: right;margin-top: 20px">
+                <el-link :underline="false" style="font-size: 16px">立即购买</el-link>
+              </div>
+
+            </el-card>
+          </el-row>
+          <br><br>
+          <el-row>
+            <div style="font-size: 38px;float: left;">促销信息</div>
+          </el-row>
+          <br>
+          <el-row>
+            <el-col>
+              <el-carousel :interval="7000" direction="vertical" height="120px" indicator-position="none">
+                <el-carousel-item v-for="(dis,i) in discountList" :key="i"
+                  style="background: #d3dce6;">
+                  <el-col :span="8">
+                    <img style="width: 25%;float: right;margin-top: 20px;margin-right:20px"
+                         :src="'/static/discount/promotion.png'">
+                  </el-col>
+                  <el-col :span="16">
+                    <span style="float: left;margin-top: 40px;font-size: 32px">礼品卡到期时间{{dis.timeName}}</span>
+                    <span
+                      style="float: left;margin-top: 40px;font-size: 32px;color:#a4d007;background:#4c6b22;display:inline-block;padding:0 6px">
+                      -{{100 - dis.discountSell * 100}}%
+                    </span>
+                  </el-col>
+
+                </el-carousel-item>
+              </el-carousel>
+            </el-col>
+          </el-row>
         </el-col>
       </el-col>
     </el-row>
@@ -54,14 +98,26 @@ export default {
   data () {
     return {
       imgList: [],
-      hotSellList: []
+      recentItemList: [],
+      hotSellList: [],
+      discountList: [],
+      itemTypes: []
     }
   },
   mounted () {
     this.loadImgList()
     this.loadHotSellList()
+    this.loadRecentItemList()
+    this.loadDiscountList()
   },
   methods: {
+    loadDiscountList () {
+      this.$axios.get('/items/discount/ads').then(resp => {
+        if (resp && resp.status === 200) {
+          this.discountList = resp.data
+        }
+      })
+    },
     loadImgList () {
       this.imgList = [
         {url: '../../static/index/welcome.png'},
@@ -72,6 +128,13 @@ export default {
       this.$axios.get('/items/hot').then(resp => {
         if (resp && resp.status === 200) {
           this.hotSellList = resp.data
+        }
+      })
+    },
+    loadRecentItemList () {
+      this.$axios.get('/items/recent').then(resp => {
+        if (resp && resp.status === 200) {
+          this.recentItemList = resp.data
         }
       })
     }
@@ -107,4 +170,5 @@ export default {
     width: 100%;
     display: block;
   }
+
 </style>
